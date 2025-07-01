0506761e72cea86191cb53aedbf8de81fbedcc86
AsifAliDeveloper <letmeetasif@gmail.com>
2025-07-01 16:25:40 +0500

    update class to className and add key

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -3,14 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  let foodItems = ["Dal", "vegetables", "Roti", "Slad", "Milk"];
+  let foodItems = ["Dal", "vegetables", "Roti", "Slad", "Milk", "Ghee"];
 
   return (
     <React.Fragment>
       <h1>Healthy Foods</h1>
-      <ul class="list-group">
+      <ul className="list-group">
         {foodItems.map((items) => (
-          <li class="list-group-item">{items}</li>
+          <li key={items} className="list-group-item">
+            {items}
+          </li>
         ))}
       </ul>
     </React.Fragment>
